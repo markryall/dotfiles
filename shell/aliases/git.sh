9b31949690247f40389c4d603c75502a7064a81b
Mark Ryall <mark@ryall.name>
2015-01-05 15:43:26 +1000

    a few convenience functions for git

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -1,10 +1,18 @@
 alias g="git status"
 alias ga="git add -A ."
+alias gb="git branch -a"
 alias gl="git log"
 alias gd="git diff"
 alias gds="git diff --staged"
 alias gsl="git stash list"
 alias gsc="git stash clear"
+
+gup() {
+  git co master
+  git fetch --prune
+  git pull
+  destroy_merged_branches
+}
 
 commit() {
   git commit -m "$*"
