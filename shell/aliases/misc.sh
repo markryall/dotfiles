3f7e7018e88f231eac2d936ce500f86802327479
Mark Ryall <mark@ryall.name>
2015-10-15 09:10:11 +1100

    a ridiculous number of aliases for various editors

--- shell/aliases/misc.sh
+++ shell/aliases/misc.sh
@@ -1,15 +1,23 @@
 alias serve_this="open http://localhost:8000 && python -m SimpleHTTPServer"
 
 alias rl="fresh && . ~/.zshrc"
+alias ze="atom ~/.zshrc"
 
 alias vim="mvim -v"
 alias vi="mvim -v"
+alias v="mvim -v"
 alias vim.="mvim -v ."
-alias vi.="mvim -v."
-
-alias mate.="mate ."
+alias vi.="mvim -v ."
+alias v.="mvim -v ."
+alias .v="mvim -v ."
 
 alias subl.="subl ."
+alias s.="subl ."
+alias .s="subl ."
+
+alias e="emacs"
+alias e.="emacs ."
+alias .e="emacs ."
 
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias vlc-rc='vlc --intf rc'
